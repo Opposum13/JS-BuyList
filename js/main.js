64ee1a1d7469ec84daeca8b2decb99cbed48f4e5
Opposum13 <vika.petruk@i.ua>
2016-01-24 19:54:16 +0200

    Add some in js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,37 @@ $(function(){
 //This	function	will	be	called
 //	after	the	DOM	was	loaded	and	ready
 //Here	we	can	place	all	jQuery	Code
-    $('button').click(function(){
+
+    var numberOfNames=3;
+    var product = {
+        name:"",
+        amount:1
+    };
+
+
+    $("div.newName").hide();
+
+
+
+
+
+    $('button.addButton').click(function(){
         $(this).css('font-size','25px');
     });
+
+    $('span.title').click(function(){        
+        $(this).hide();
+        $("div.newName").show();
+    });
+
+
+
+
+
+
+
+
+
+
+
 });
