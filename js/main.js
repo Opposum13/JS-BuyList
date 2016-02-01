c0abeb86f3088b0b51b04cb970dd2bf57d83af8f
Opposum13 <vika.petruk@i.ua>
2016-02-01 20:49:05 +0200

    Strange function(

--- js/main.js
+++ js/main.js
@@ -7,40 +7,45 @@ $(function(){
 //Here	we	can	place	all	jQuery	Code
 
 
-    $('button.addButton').click(function(){
-        var newElem = $("#inp").val();
-        if(newElem!==""){
-            $("#inp").val("");
-            $("div.left").append('<div class="d3 d4 ">'+
-                '<div class="elem t">'+
-                ' <span class="title ">'+newElem+'</span>'+
-                ' </div>'+
-                '<div class="elem padL5">'+
-                '<button class="circButton min plMinButt te" >'+
-                '<i class="minicon">-</i>'+
-                '</button>'+
-                '<span class="numb pad5">1</span>'+
-                '<button class="circButton pl plMinButt te">'+
-                '<i class="plusicon">+</i>'+
-                '</button>'+
-                '</div>'+
-                '<div class="elem te2 pad50">'+
-                '<button class="boughtButt margR5">Куплено</button>'+
-                '<button class="deleteButton" title="Видалити" style="margin-left: 5px;">'+
-                '<i class="remove icon"></i>'+
-                '</button>'+
-                '</div>'+
-                '</div>');
+
+    //First version without node
 
 
-            $("div.notB").append('<div class="notBoughtElements margT">'+
-                '<span class="title"><b>'+newElem+'</b></span>'+
-                '<span class="miniLabel"><b>1</b></span>'+
-            '</div>');
-        }
 
-
-    });
+//    $('button.addButton').click(function(){
+//        var newElem = $("#inp").val();
+//        if(newElem!==""){
+//            $("#inp").val("");
+//            $("div.left").append('<div class="d3 d4 ">'+
+//                '<div class="elem t">'+
+//                ' <span class="title ">'+newElem+'</span>'+
+//                ' </div>'+
+//                '<div class="elem padL5">'+
+//                '<button class="circButton min plMinButt te" >'+
+//                '<i class="minicon">-</i>'+
+//                '</button>'+
+//                '<span class="numb pad5">1</span>'+
+//                '<button class="circButton pl plMinButt te">'+
+//                '<i class="plusicon">+</i>'+
+//                '</button>'+
+//                '</div>'+
+//                '<div class="elem te2 pad50">'+
+//                '<button class="boughtButt margR5">Куплено</button>'+
+//                '<button class="deleteButton" title="Видалити" style="margin-left: 5px;">'+
+//                '<i class="remove icon"></i>'+
+//                '</button>'+
+//                '</div>'+
+//                '</div>');
+//
+//
+//            $("div.notB").append('<div class="notBoughtElements margT">'+
+//                '<span class="title"><b>'+newElem+'</b></span>'+
+//                '<span class="miniLabel"><b>1</b></span>'+
+//            '</div>');
+//        }
+//
+//
+//    });
 
     $('button.deleteButton').click(function(){
         var name=$(this).parent().find(".elem").find(".title").text();
@@ -92,7 +97,69 @@ $(function(){
 
 
 
+//    New version with node
 
+
+
+    function createNewElement(newElem){
+
+        var node ='<div class="d3 d4 ">'+
+            '<div class="elem t">'+
+            ' <span class="title ">'+newElem+'</span>'+
+            ' </div>'+
+            '<div class="elem padL5">'+
+            '<button class="circButton min plMinButt te" >'+
+            '<i class="minicon">-</i>'+
+            '</button>'+
+            '<span class="numb pad5">1</span>'+
+            '<button class="circButton pl plMinButt te">'+
+            '<i class="plusicon">+</i>'+
+            '</button>'+
+            '</div>'+
+            '<div class="elem te2 pad50">'+
+            '<button class="boughtButt margR5">Куплено</button>'+
+            '<button class="deleteButton" title="Видалити" style="margin-left: 5px;">'+
+            '<i class="remove icon"></i>'+
+            '</button>'+
+            '</div>'+
+            '</div>';
+
+
+        var node2 ='<div class="notBoughtElements margT">'+
+            '<span class="title"><b>'+newElem+'</b></span>'+
+            '<span class="miniLabel"><b>1</b></span>'+
+            '</div>';
+
+
+
+
+
+//        node.find('.deleteButton').click(function () {
+////            $("div.left").hide();
+////            var name=$(this).parent().find(".elem").find(".title").text();
+////            $(this).parent().parent().remove();
+//        });
+
+
+        $("div.left").append(node);
+        $("div.notB").append(node2);
+    }
+
+
+
+
+
+
+    $('button.addButton').click(function(){
+        var newElem = $("#inp").val();
+        if(newElem!==""){
+
+            createNewElement(newElem);
+            $("#inp").val("");
+        }
+
+
+    });
 
 
 
